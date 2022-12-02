f5b20e92781db8c93b7241a22d19a63a8e673413
Dhanush N <71968114+RorYin@users.noreply.github.com>
2022-12-03 01:46:03 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-token = "5851812153:AAFqyJ5lgaxIrWJhC6HDCKIGzXN_ojncgVs" #testing
+token = "5002606701:AAFt2QcHWaEgqW0T63dXmTIQYglYX2Dw1bM" #testing
 # key = Fernet.generate_key()
 key = "P0mpAZyn3WGpj88GFGewWwmY4OI4JxZyxeJ3-UKUH6A="
 
 airtablekey = "key6OhVcVppxONYOe"
 
 devTGid = 887572477
+
+BaseId = "appHXjkKNtDVs9aVm"
+
+TableId = "tblXkWAm9VWZ5MdI8"
+
+TableName = "V1Data"
